e0e40ed7bf1961d2845b37d4dcbc4d3b4bb2d9a7
Jansen <13516011@std.stei.itb.ac.id>
2019-05-05 00:53:11 +0700

    Updates

---
+++ precache-manifest.b6e2ad2baa2b79e9220fed4e90a54857.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd60f3290c43c4707849432d7f7c03f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "134bfdeb4fae81933445",
+    "url": "/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "021bf70a4b87f983fc73",
+    "url": "/static/css/main.1b0a56bf.chunk.css"
+  },
+  {
+    "revision": "134bfdeb4fae81933445",
+    "url": "/static/js/2.ba39a63c.chunk.js"
+  },
+  {
+    "revision": "021bf70a4b87f983fc73",
+    "url": "/static/js/main.9892ecf7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3815a9caf31546211741bfe21fa828d5",
+    "url": "/static/media/face-recognition.3815a9ca.jpg"
+  },
+  {
+    "revision": "7e9b4597c36578945fc637a3c06bd498",
+    "url": "/static/media/me.7e9b4597.jpg"
+  },
+  {
+    "revision": "ee57e66e5709b4c32064f623368a97c4",
+    "url": "/static/media/visual-studio.ee57e66e.png"
+  }
+]);
--- src/components/Greet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-// function Greet() {
-//     return <h1>Greetings</h1>
-// }
-
-const Greet = (props) => <h1>Hello {props.name}</h1>
-
-export default Greet
